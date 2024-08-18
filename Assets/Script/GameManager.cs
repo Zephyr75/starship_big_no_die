8cd7c017d0d26f1a3ae77b2afbb5be7728454504
Antoine <antoine.familleroger@gmail.com>
2024-08-18 12:20:54 +0200

    Added editor

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Script;
@@ -14,6 +15,13 @@ public class GameManager : MonoBehaviour
     private SerializableDictionary<ComponentsEnum, GameObject> myMap;
     
     private StarShips ship;
+    
+    [SerializeField]
+    private Material previewMaterial;
+
+    private GameObject preview;
+    private Transform oldHit;
+    private Vector3 oldNormal;
     
     void Start()
     {
@@ -44,6 +52,81 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Time.timeScale > 0.0f)
+        {
+            if (preview)
+            {
+                Destroy(preview);
+            }
+            return;
+        }
         
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if (hit.transform != oldHit || oldNormal != hit.normal)
+            {
+                Destroy(preview);
+                Transform hitTransform = hit.transform;
+                Vector3 ortho = hitTransform.forward;
+                if (Vector3.Dot(ortho, hit.normal) != 0)
+                {
+                    ortho = hitTransform.transform.up;
+                }
+                preview = Instantiate(prefabsComponents[ComponentsEnum.Canon], hit.transform.position + hit.normal,
+                    Quaternion.LookRotation(hit.normal, ortho));
+                Renderer previewRenderer = preview.GetComponent<Renderer>();
+                Material[] materials = previewRenderer.materials;
+                for (int i = 0; i < materials.Length; i++)
+                {
+                    materials[i] = previewMaterial;
+                }
+                previewRenderer.materials = materials;
+                Destroy(preview.GetComponent<Collider>());
+                oldHit = hit.transform;
+                oldNormal = hit.normal;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && preview)
+        {
+            GameObject newComponent = Instantiate(prefabsComponents[ComponentsEnum.Canon], preview.transform.position,
+                preview.transform.rotation);
+            ComponentSS comp = newComponent.GetComponent<ComponentSS>();
+            print(getDirection(preview.transform.forward));
+            comp.SetDir(getDirection(preview.transform.forward));
+            if (comp != null)
+            {
+                ship.AddComponents(comp);
+            }
+        }
     }
+
+    DirEnum getDirection(Vector3 forward)
+    {
+        Transform controller = ship.GetController().transform;
+        if (forward == controller.forward)
+        {
+            return DirEnum.Front;
+        }
+        if (forward == -controller.forward)
+        {
+            return DirEnum.Behind;
+        }
+        if (forward == controller.up)
+        {
+            return DirEnum.Up;
+        }
+        if (forward == -controller.up)
+        {
+            return DirEnum.Down;
+        }
+        if (forward == controller.right)
+        {
+            return DirEnum.Right;
+        }
+        return DirEnum.Left;
+    }
+
 }
--- Assets/Script/MainCamera.cs
+++ Assets/Script/MainCamera.cs
@@ -4,18 +4,88 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
+    [SerializeField]
+    private Transform target;
     
-    private Vector3 vect = new Vector3(0, 1, -2);
-    // Start is called before the first frame update
+    private float distance = 5.0f;
+    private float xSpeed = 500.0f;
+    private float ySpeed = 500.0f;
+
+    private float yMinLimit = -80f;
+    private float yMaxLimit = 80f;
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+    
+    private float deltaTime = 0.0f;
+    private float lastTime = 0.0f;
+    
+    private Vector3 ratio = new Vector3(0, 0.5f, -1);
+    private bool isEditing;
+
+
     void Start()
     {
-        transform.localPosition = vect*3;
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
 
+        transform.position = target.position - transform.forward * distance;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //transform.position = vect;
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            isEditing = !isEditing;
+            Time.timeScale = isEditing ? 0.0f : 1.0f;
+            print("Switched mode");
+
+            if (isEditing)
+            {
+                transform.parent = target.parent;
+            }
+            else
+            {
+                transform.parent = target;
+                transform.localPosition = ratio * distance;
+                transform.localEulerAngles = new Vector3(26.6f, 0, 0);
+            }
+        }
+        
+        float newTime = Time.realtimeSinceStartup;
+        deltaTime = newTime - lastTime;
+        lastTime = newTime;
+        
+        if (Input.GetMouseButton(1) && Time.timeScale == 0.0f)
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * deltaTime;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * deltaTime;
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+            Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+
+        if (Time.timeScale > 0.0f)
+        {
+            transform.localPosition = ratio * distance;
+            transform.localEulerAngles = new Vector3(26.6f, 0, 0);
+        }
+
+        
+    }
+
+    private float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
     }
 }
